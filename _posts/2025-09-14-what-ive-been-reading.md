f8802727807849fa1d81bf9c60c26962265e6ec6
wavdl <bvanderlaar@proton.me>
2025-09-14 23:12:14 -0400

    Added my reading list to the site, as well as various fixes/cleanups.

---
+++ _posts/2025-09-14-what-ive-been-reading.md
@@ -0,0 +1,16 @@
+---
+layout: post
+author: William Van Der Laar
+title: "What I've Been Reading Lately"
+date: 2025-09-14 10:00:00 -0500
+published: true
+---
+
+Updated daily from my [StoryGraph](https://app.thestorygraph.com/profile/wavdl) and [Pinboard](https://pinboard.in/u:Wavdl) profiles.
+
+## On The Web
+{% include bookmarks.html %}
+
+## Books
+{% include books.html %}
+
--- _scripts/PullUpdates.py
+++ _scripts/PullUpdates.py
@@ -1,5 +1,5 @@
 from RlStatsScraper import RlStatsScraper, Platform
-from storygraph_api import User
+from storygraph_api import User, Book
 import pinboard
 import json
 import datetime
@@ -9,7 +9,13 @@ def ranks_to_json(filename):
     scraper = RlStatsScraper(Platform.STEAM, "wavdl")
     output = dict()
     output['lastUpdatedTime'] = str(datetime.datetime.now())
-    output['ranks'] = scraper.get_ranks()
+    scraped_ranks = scraper.get_ranks()
+    ranks = []
+    for rank in scraped_ranks:
+        if 'Tournament' in rank.playlist:
+            continue
+        ranks.append(rank)
+    output['ranks'] = ranks
     with open(filename, 'w') as f:
         json.dump(output, f, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
@@ -17,16 +23,20 @@ def books_to_json(filename, cookie):
     output = dict()
     user = User()
     read_books_str = user.books_read('wavdl', cookie=cookie)
-    read_books = json.loads(read_books_str)
+    read_books = json.loads(read_books_str)[:15]
+    book = Book()
+    for read_book in read_books:
+        info_str = book.book_info(read_book['book_id'])
+        info = json.loads(info_str)
+        read_book['authors'] = info['authors']
     output['read'] = read_books[:15]
     with open(filename, 'w') as f:
         json.dump(output, f, indent=4)
 
 def links_to_json(filename, api_key):
     pb = pinboard.Pinboard(api_key)
-    output = dict()
     posts = pb.posts.recent(tag=["reading-list", "python"])
-    bookmarks = posts['posts']
+    bookmarks = posts['posts'][:10]
     recents = []
     for bookmark in bookmarks:
         entry = {
@@ -35,7 +45,7 @@ def links_to_json(filename, api_key):
         'url': bookmark.url
         }
         recents.append(entry)
-    output['recent'] = recents
+    output = {'recent': recents}
     with open(filename, 'w') as f:
         json.dump(output, f, indent=4)
 
